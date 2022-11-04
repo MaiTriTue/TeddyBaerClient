138ad1f7e51caed642dabc901271d26d19819e9a
Tri Tue Mai <HacTinhThan@gmail.com>
2022-11-05 00:05:21 +0700

    commit lan 3

--- src/App.js
+++ src/App.js
@@ -1,56 +1,50 @@
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import { Fragment } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import { PublicRoutes } from '~/routes';
-import { DefaultLayout } from '~/components/Layout';
-
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    {PublicRoutes.map((route, index) => {
-                        const Page = route.component;
-                        const Layout = DefaultLayout;
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
-            </div>
-        </Router>
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
     );
 }
 
 export default App;
+
+// import { Fragment } from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import { PublicRoutes } from '~/routes';
+// import { DefaultLayout } from '~/components/Layout';
+
+// function App() {
+//     return (
+//         <Router>
+//             <div className="App">
+//                 <Routes>
+//                     {PublicRoutes.map((route, index) => {
+//                         const Page = route.component;
+//                         const Layout = DefaultLayout;
+//                         return (
+//                             <Route
+//                                 key={index}
+//                                 path={route.path}
+//                                 element={
+//                                     <Layout>
+//                                         <Page />
+//                                     </Layout>
+//                                 }
+//                             />
+//                         );
+//                     })}
+//                 </Routes>
+//             </div>
+//         </Router>
+//     );
+// }
+
+// export default App;
--- src/components/Layout/index.js
+++
@@ -1,4 +0,0 @@
-export { default as DefaultLayout } from './DefaultLayout';
-export { default as HeaderOnly } from './HeaderOnly';
-export { default as Header } from './componentsLayout/Header';
-export { default as Footer } from './componentsLayout/Footer';
